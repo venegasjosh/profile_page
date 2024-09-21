3be9cb700116bdc3d7b83a90df1ba4e4f08a713b
Joshua J Venegas <47758513+venegasjosh@users.noreply.github.com>
2024-09-21 15:07:02 -0500

    fixed email form

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,6 +1,6 @@
 function sendEmail() {
     // console.log("SLDFKJS:LF")
-    
+    console.log("here")
     let nameValue = document.getElementById("name1").value;
     let emailValue = document.getElementById("email1").value;
     let subjectValue = document.getElementById("subject1").value;
@@ -10,7 +10,7 @@ function sendEmail() {
 	Email.send({
 	Host: "smtp.elasticemail.com",
 	Username : "venegasjosh@gmail.com",
-	Password : "dfab1899-481a-49a2-ab32-7e99dc2f890f",
+	Password : "4b6475cd-9b13-4431-abbf-e2a959e86c8b",
 	To : "venegasjosh@gmail.com",
 	From : emailValue.toString(),
 	Subject : nameValue.toString() + ' : '+ subjectValue.toString(),
