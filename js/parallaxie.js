78e813bcc7275f9025e2022c42bf74c4aaa7ea8c
Joshua J Venegas <47758513+venegasjosh@users.noreply.github.com>
2024-09-24 02:17:49 -0500

    try 10

--- js/parallaxie.js
+++ js/parallaxie.js
@@ -35,9 +35,9 @@
                 var pos_y =  local_options.offset + ($el.offset().top - $(window).scrollTop()) * (1 - local_options.speed );
                 $el.css({
                     'background-image': image_url,
-                    // 'background-size': local_options.size,
+                    'background-size': local_options.size,
                     'background-repeat': local_options.repeat,
-                    // 'background-attachment': 'fixed',
+                    'background-attachment': 'absolute',
                     'background-position': local_options.pos_x + ' ' + pos_y + 'px',
                 });
 
